71b9ef9aa2befb4d66ca5fdc600463de17e437e4
123 <31>
2019-07-19 16:56:36 +0700

    add login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -16,7 +18,7 @@ class LoginController extends Controller
     | redirecting them to your home screen. The controller uses a trait
     | to conveniently provide its functionality to your applications.
     |
-    */
+     */
 
     use AuthenticatesUsers;
 
@@ -25,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
@@ -35,5 +37,10 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return redirect('/login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/matakuliah', 'matakuliahController@index')->name('matakuliah');
-Route::get('/jurusan', 'jurusanController@index')->name('jurusan');
-Route::get('/asisten', 'asistenController@index')->name('asisten');
-Route::get('/dashboard', 'dashboardController@index')->name('dashboard_');
-Route::post('/dashboard', 'dashboardController@store')->name('dashboard');
-Route::get('/presensi', 'presensiController@index')->name('presensi');
-Route::get('/download/{kode_jurusan}/{jenis}', 'presensiController@downloadExcel')->name('download');
-Route::get('/update-presensi/{id}/{tgl_mulai}/{tgl_selesai}/{is_aktif}','presensiController@update')->name('presensi.update');
-Route::get('/insert-presensi','presensiController@create')->name('presensi.create');
-Route::post('/store-presensi','presensiController@store')->name('presensi.store');
-Route::get('/destroy-presensi/{id}','presensiController@destroy')->name('presensi.destroy');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/matakuliah', 'matakuliahController@index')->name('matakuliah');
+    Route::get('/jurusan', 'jurusanController@index')->name('jurusan');
+    Route::get('/asisten', 'asistenController@index')->name('asisten');
+    Route::get('/dashboard', 'dashboardController@index')->name('dashboard_');
+    Route::post('/dashboard', 'dashboardController@store')->name('dashboard');
+    Route::get('/presensi', 'presensiController@index')->name('presensi');
+    Route::get('/download/{kode_jurusan}/{jenis}', 'presensiController@downloadExcel')->name('download');
+    Route::get('/update-presensi/{id}/{tgl_mulai}/{tgl_selesai}/{is_aktif}', 'presensiController@update')->name('presensi.update');
+    Route::get('/insert-presensi', 'presensiController@create')->name('presensi.create');
+    Route::post('/store-presensi', 'presensiController@store')->name('presensi.store');
+    Route::get('/destroy-presensi/{id}', 'presensiController@destroy')->name('presensi.destroy');
 
 //insert data asisten
-Route::get('/input-data','inputDataController@index')->name('input');
-Route::post('/input-single','inputDataController@input_single')->name('input.single');
-Route::post('/input-collection','inputDataController@csvfileupload')->name('input.collection');
+    Route::get('/input-data', 'inputDataController@index')->name('input');
+    Route::post('/input-single', 'inputDataController@input_single')->name('input.single');
+    Route::post('/input-collection', 'inputDataController@csvfileupload')->name('input.collection');
 //remove data asisten
-Route::get('/remove-data','removeDataController@index')->name('remove');
-Route::post('/remove-single','removeDataController@remove_single')->name('remove.single');
-Route::post('/remove-collection','removeDataController@csvfileupload')->name('remove.collection');
+    Route::get('/remove-data', 'removeDataController@index')->name('remove');
+    Route::post('/remove-single', 'removeDataController@remove_single')->name('remove.single');
+    Route::post('/remove-collection', 'removeDataController@csvfileupload')->name('remove.collection');
+});
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
 Auth::routes();
 
